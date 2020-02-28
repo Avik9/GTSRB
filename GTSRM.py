2c9e9edcf8b66010455407e277c08ae5b86cbd5e
Avik Kadakia <avik.kadakia@stonybrook.edu>
2020-02-28 13:13:53 -0500

    First commit

---
+++ GTSRM.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pickle
+
+class Data_Set_Loader():
+
+    train_set = None
+    test_set = None
+
+    def __init__(self, test_dir, train_dir):
+        pass
