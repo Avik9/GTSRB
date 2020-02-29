4879ce5210e34190d7ca10d09cf5ab8cd74defca
Avik Kadakia <avik.kadakia@stonybrook.edu>
2020-02-28 21:21:11 -0500

    Loads the train and test images into the np.array

--- GTSRM.py
+++ GTSRM.py
@@ -1,38 +1,48 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import pickle
 import cv2
 from os import listdir, walk
 from os.path import isfile, join
 
+
 class Data_Set_Loader():
 
-    train_set = None
-    test_set = None
+    x_train_set = []
+    y_train_set = []
+    x_test_set = []
+    y_test_set = []
 
-    def __init__(self):
-        pass
+    def __init__(self, training_path, testing_path):
+            
+        counter = 0
+
+        for path, subdirs, files in walk(training_path):
+            for name in files:
+                if ".ppm" in name:
+                    counter += 1
+                    image = cv2.imread(join(path, name), flags=cv2.IMREAD_COLOR)
+                    self.x_train_set.append(image)
+
+        print("Num Training Files:", counter)
+
+        counter = 0
+
+        for path, subdirs, files in walk(testing_path):
+            for name in files:
+                if ".ppm" in name:
+                    counter += 1
+                    image = cv2.imread(join(path, name), flags=cv2.IMREAD_COLOR)
+                    self.x_test_set.append(image)
+
+        print("Num Testing Files:", counter)
+
 
 if __name__ == "__main__":
 
     training_path = 'Training'
 
-    loader = Data_Set_Loader()
+    loader = Data_Set_Loader('Training', 'Testing')
 
-    # loader.train_set = pickle.load(open(training_path, 'rb'))
-    # loader.x_train, loader.y_train = loader.train_set['features'], loader.train['labels']
-
-    # onlyfiles = [f for f in listdir(training_path) if isfile(join(training_path, f))]
-
-    counter = 0
-
-    for path, subdirs, files in walk(training_path):
-        for name in files:
-            print (join(path, name))
-            if ".ppm" in name:
-                counter += 1
-
-    print("Num Files:", counter)
-
-    # print(onlyfiles)
+    # print(loader.train_set[32])
+    # print(len(loader.train_set))
