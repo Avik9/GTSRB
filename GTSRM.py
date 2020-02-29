7c11358d549c22f8760220902db582fb3cb1a58d
Avik Kadakia <avik.kadakia@stonybrook.edu>
2020-02-29 02:04:10 -0500

    Batch iterator does not let the process end

--- GTSRM.py
+++ GTSRM.py
@@ -1,27 +1,38 @@
-import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tqdm import tqdm
+import numpy as np
 import cv2
 import csv
-import matplotlib.pyplot as plt
-from os import listdir, walk
-from os.path import isfile, join
+import os
 
 
 class Data_Set_Loader():
 
     x_train_set = []
     y_train_set = []
+
     x_test_set = []
     y_test_set = []
 
+    X_batch = []
+    Y_batch = []
+
+    tf_sess = None
+
+    batch_size = 512 # Combines the number of elements into 1 batch     https://www.tensorflow.org/api_docs/python/tf/data/Dataset#batch
+    repeat_size = 5 # How many times each value is seen                 https://www.tensorflow.org/api_docs/python/tf/data/Dataset#repeat
+    shuffle = 42 # randomly selects the number of buffer_size element   https://www.tensorflow.org/api_docs/python/tf/data/Dataset#shuffle
+
     def __init__(self, training_path, testing_path):
 
+        print("TensorFlow Version: ", tf.__version__)
+        self.tf_sess = tf.Session()
         self.load_data(training_path, testing_path)
-        self.display_one(self.x_train_set[1100])
+        self.x_train_set = self.preprocess(self.x_train_set)
+        # self.batch_iterator(self.x_train_set, self.y_train_set)
 
-    # Loads x_train, x_test, y_train, y_test into the class variables
-
+    # Loads x_train, x_test, y_train, y_test into the class variables.
     def load_data(self, training_path, testing_path):
 
         counter = 0
@@ -29,27 +40,26 @@ class Data_Set_Loader():
         image_name = {}
         full_path = ''
 
-        for path, subdirs, files in walk(training_path):
+        for path, subdirs, files in os.walk(training_path):
             for name in files:
                 if ".ppm" in name:
-                    full_path = join(path, name)
+                    full_path = os.path.join(path, name)
                     image_name[full_path] = image_counter
                     counter += 1
                     image_counter += 1
                     image = cv2.imread(full_path, flags=cv2.IMREAD_COLOR)
                     self.x_train_set.append(image)
 
-        for path, subdirs, files in walk(training_path):
+        for path, subdirs, files in os.walk(training_path):
             for name in files:
                 if '.csv' in name:
-                    with open(join(path, name)) as csv_file:
+                    with open(os.path.join(path, name)) as csv_file:
                         csv_reader = csv.reader(csv_file, delimiter=";")
                         row_count = 0
                         for row in csv_reader:
                             if(row_count > 0):
-                                full_path_row = join(path, row[0])
-                                self.y_train_set.append(
-                                    image_name[full_path_row])
+                                full_path_row = os.path.join(path, row[0])
+                                self.y_train_set.append(row[7])
                             row_count += 1
 
         # print("Num Training Images:", counter)
@@ -58,35 +68,33 @@ class Data_Set_Loader():
 
         counter = 0
 
-        for path, subdirs, files in walk(testing_path):
+        for path, subdirs, files in os.walk(testing_path):
             for name in files:
                 if ".ppm" in name:
-                    full_path = join(path, name)
+                    full_path = os.path.join(path, name)
                     image_name[full_path] = image_counter
                     counter += 1
                     image_counter += 1
                     image = cv2.imread(full_path, flags=cv2.IMREAD_COLOR)
                     self.x_test_set.append(image)
 
-        for path, subdirs, files in walk(testing_path):
-            for name in files:
-                if '.csv' in name:
-                    with open(join(path, name)) as csv_file:
-                        csv_reader = csv.reader(csv_file, delimiter=";")
-                        row_count = 0
-                        for row in csv_reader:
-                            if(row_count > 0):
-                                full_path_row = join(path, row[0])
-                                self.y_test_set.append(
-                                    image_name[full_path_row])
-                            row_count += 1
+        # for path, subdirs, files in os.walk(testing_path):
+        #     for name in files:
+        #         if '.csv' in name:
+        #             with open(os.path.join(path, name)) as csv_file:
+        #                 csv_reader = csv.reader(csv_file, delimiter=";")
+        #                 row_count = 0
+        #                 for row in csv_reader:
+        #                     if(row_count > 0):
+        #                         full_path_row = os.path.join(path, row[0])
+        #                         self.y_test_set.append(row[7])
+        #                     row_count += 1
 
         # print("\nNum Training Images:", counter)
         # print("X_Test_set:", len(self.x_test_set))
-        # print("Y_Test_Set:", len(self.y_test_set))
+        # # print("Y_Test_Set:", len(self.y_test_set))
 
-    # Displays the image sent in
-
+    # Displays the image sent in.
     def display_one(self, a, title1="Original"):
 
         plt.imshow(a)
@@ -95,9 +103,43 @@ class Data_Set_Loader():
         plt.yticks([])
         plt.show()
 
+    # Processes each RGB pixel by dividing its value by 255 and saving it in [0, 1) format.
+    def preprocess(self, features):
+        return_vector = []
+
+        for image in tqdm(features):
+            image = np.float32(np.divide(image, 255))
+            return_vector.append(image)
+
+        return return_vector
+
+    # Iterates through the batch to make repeat the signs, shuffle them, and select batches.
+    def batch_iterator(self, features, labels):
+        x_set = tf.data.Dataset.from_tensor_slices(features)
+        y_set = tf.data.Dataset.from_tensor_slices(labels)
+        data = tf.data.Dataset.zip((x_set, y_set)).batch(2)
+
+        # data = data.repeat(self.repeat_size)
+        # data = data.shuffle(self.shuffle)
+        # data = data.batch(self.batch_size)
+        # iterator = tf.Data.Iterator.from_structure(data.output_types, data.output_shapes)
+        # train_init = iterator.make_initializer(data)
+        # self.X_batch, self.Y_batch = iterator.get_next()
+
+
 
 if __name__ == "__main__":
 
-    training_path = 'Training'
+    loader = Data_Set_Loader(training_path='./Training', testing_path='./Testing')
 
-    loader = Data_Set_Loader('Training', 'Testing')
+    n_train = len(loader.x_train_set)
+    n_test = len(loader.x_test_set)
+    x_n_iterator = len(loader.X_batch)
+    y_n_iterator = len(loader.Y_batch)
+    n_classes = len(set(loader.y_train_set))
+
+    print("Number of training examples =", n_train)
+    print("Number of testing examples =", n_test)
+    print("Number of iterator X examples =", x_n_iterator)
+    print("Number of iterator Y examples =", y_n_iterator)
+    print("Number of classes =", n_classes)
