1ae6e289892123aaa68e1b20ab86c1406101e4ce
Avik Kadakia <avik.kadakia@stonybrook.edu>
2020-03-04 01:03:17 -0500

    Wrote the batch iterator method. Works perfectly.

--- GTSRB.py
+++ GTSRB.py
@@ -6,6 +6,7 @@ import cv2
 import csv
 import os
 from PIL import Image
+import random
 
 
 class Data_Set_Loader():
@@ -19,19 +20,24 @@ class Data_Set_Loader():
     X_batch = []
     Y_batch = []
 
+    X_batch_returned = []
+    Y_batch_returned = []
+
+    X_batch_used = []
+
     tf_sess = None
 
-    batch_size = 512    # Combines the number of elements into 1 batch          https://www.tensorflow.org/api_docs/python/tf/data/Dataset#batch
+    batch_size = 128    # Combines the number of elements into 1 batch          https://www.tensorflow.org/api_docs/python/tf/data/Dataset#batch
     repeat_size = 5     # How many times each value is seen                     https://www.tensorflow.org/api_docs/python/tf/data/Dataset#repeat
     shuffle = 42        # randomly selects the number of buffer_size element    https://www.tensorflow.org/api_docs/python/tf/data/Dataset#shuffle
 
     def __init__(self, training_path, testing_path):
 
-        print("TensorFlow Version: ", tf.__version__)
-        self.tf_sess = tf.Session()
+        # print("TensorFlow Version: ", tf.__version__)
+        # self.tf_sess = tf.Session()
         self.load_data(training_path, testing_path)
         self.x_train_set = self.preprocess(self.x_train_set)
-        # self.batch_iterator(self.x_train_set, self.y_train_set)
+        self.batch_iterator(self.batch_size)
 
     # Loads x_train, x_test, y_train, y_test into the class variables.
     def load_data(self, training_path, testing_path):
@@ -65,7 +71,7 @@ class Data_Set_Loader():
 
         self.x_train_set, self.y_train_set = self.readTrafficSigns(training_path)
 
-        # print("Num Training Images:", counter)
+        print("Num Training Images:", counter)
         print("X_Train_set:", len(self.x_train_set))
         print("Y_Train_Set:", len(self.y_train_set))
 
@@ -93,9 +99,12 @@ class Data_Set_Loader():
                                 self.y_test_set.append(row[7])
                             row_count += 1
 
-        # print("\nNum Training Images:", counter)
-        # print("X_Test_set:", len(self.x_test_set))
-        # # print("Y_Test_Set:", len(self.y_test_set))
+        self.Y_batch = self.y_train_set
+        self.X_batch = self.x_train_set
+
+        print("\nNum Training Images:", counter)
+        print("X_Test_set:", len(self.x_test_set))
+        print("Y_Test_Set:", len(self.y_test_set))
 
     # Displays the image sent in.
     def display_one(self, a, title1="Original"):
@@ -117,17 +126,72 @@ class Data_Set_Loader():
         return return_vector
 
     # Iterates through the batch to make repeat the signs, shuffle them, and select batches.
-    def batch_iterator(self, features, labels):
-        x_set = tf.data.Dataset.from_tensor_slices(features)
-        y_set = tf.data.Dataset.from_tensor_slices(labels)
-        data = tf.data.Dataset.zip((x_set, y_set)).batch(2)
+    def batch_iterator(self, batch_size):
 
-        data = data.repeat(self.repeat_size)
-        data = data.shuffle(self.shuffle)
-        data = data.batch(self.batch_size)
-        iterator = tf.Data.Iterator.from_structure(data.output_types, data.output_shapes)
-        train_init = iterator.make_initializer(data)
-        self.X_batch, self.Y_batch = iterator.get_next()
+        num_batches = 0
+
+        images_used = num_images = 0
+
+        random_position = -1
+
+        while images_used < len(self.x_train_set):
+
+            positions = []                                                                                      # Positions used in this iteration
+            self.X_batch_returned = []                                                                          # Images returned per batch
+            self.Y_batch_returned = []                                                                          # Classes returned per batch
+            num_images = 0                                                                                      # Number of images read in each batch iteration
+
+            for number in range(batch_size):                                                                    # For each image to be returned in the iteration
+
+                while num_images < batch_size:                                                                  # While the number of images returned in this iteration is less than the required amount
+
+                    if batch_size > (len(self.X_batch) - images_used):                                          # If the batch size requirement is greater than the number of images left to be returned
+                        for num in range(len(self.x_train_set)):                                                # For every image in the training set 
+                            if num not in self.X_batch_used:                                                    # For every imaged not used yet
+                                if num_images < (len(self.X_batch) - images_used): 
+                                    # print("Num images:", num_images, num)
+                                    num_images += 1                                                                 
+                                    positions.append(num)
+                                    self.X_batch_returned.append(self.x_train_set[num])
+                                    self.Y_batch_returned.append(self.y_train_set[num])
+                                else:
+                                    self.X_batch_used.extend(positions)
+                                    num_batches += 1
+
+                                    images_used += len(self.X_batch_returned)
+                                    # print(len(self.X_batch_returned), "images were returned with", len(self.X_batch) - images_used, "images remaining")
+                                    # print(len(self.Y_batch_returned), "images were returned with", len(self.X_batch) - images_used, "images remaining\n")
+
+                                    # print(num_batches, "batches of images were removed")
+                                    # print(images_used, "images were removed")
+                                    return;
+                    else:
+                        random_position = random.randrange(len(self.x_train_set))
+
+                        if random_position not in self.X_batch_used and random_position not in positions:
+                            num_images += 1
+                            positions.append(random_position)
+                            self.X_batch_returned.append(self.x_train_set[random_position])
+                            self.Y_batch_returned.append(self.y_train_set[random_position])
+
+            self.X_batch_used.extend(positions)
+            num_batches += 1
+
+            images_used += len(self.X_batch_returned)
+        
+            # print(len(self.X_batch_returned), "images were returned with", len(self.X_batch) - images_used, "images remaining")
+            # print(len(self.Y_batch_returned), "images were returned with", len(self.X_batch) - images_used, "images remaining\n")
+
+        # x_set = tf.data.Dataset.from_tensor_slices(features)
+        # y_set = tf.data.Dataset.from_tensor_slices(labels)
+        # data = tf.data.Dataset.zip((x_set, y_set)).batch(2)
+
+        # data = data.repeat(self.repeat_size)
+        # data = data.shuffle(self.shuffle)
+        # data = data.batch(self.batch_size)
+        # iterator = tf.Data.Iterator.from_structure(data.output_types, data.output_shapes)
+        # train_init = iterator.make_initializer(data)
+        # self.X_batch, self.Y_batch = iterator.get_next()
 
     # function for reading the images
     # arguments: path to the traffic sign data, for example './GTSRB/Training'
